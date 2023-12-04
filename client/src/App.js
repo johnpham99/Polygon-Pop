747f27f0691b4f90af9061d8fb6d200b0b7e27c1
John Pham <johnpham456@gmail.com>
2023-12-04 17:56:59 -0500

    Initialize 9x9 board with random values
    values range from [0,4]
    clicking on a cell makes the value "X"

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,51 @@
-import React, {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react'
 
-function App() {
-
-  const [backendData, setBackendData] = useState([{}])
-  
-  useEffect(() => {
-    fetch("/play").then(
-      response => response.json()
-    ).then(
-      data => {
-        setBackendData(data)
-      }
-    )
-  }, [])
-
-  return (
-    <div>
-      {(typeof backendData.users === 'undefined') ? (
-        <p>Loading...</p>
-      ): (
-        backendData.users.map((user,i) => (
-          <p key={i}>{user}</p>
-        ))
-      )}
-    </div>
-  )
+function Square({value, onSquareClick}) {
+  return <button className="square" onClick = {onSquareClick}>
+    {value}
+  </button>
 }
 
-export default App
+export default function Board() {
+  const [squares, setSquares] = useState(Array(81).fill(null))
+
+  useEffect(() => {
+    randomizeBoard(); 
+  }, []); 
+
+  function randomizeBoard() {
+    const nextSquares = squares.slice()
+
+    for (let r = 0; r < squares.length; r++) {
+      for (let c = 0; c < squares.length; c++) {
+        const randomNumber = Math.floor(Math.random() * 5);
+        nextSquares[r*9+c] = randomNumber.toString()
+      }
+    }
+    setSquares(nextSquares)
+  }
+  
+  function handleClick(i) {
+    const nextSquares = squares.slice()
+    nextSquares[i] = "X"
+    setSquares(nextSquares)
+  }
+
+  function renderSquare(i) {
+    return <Square value={squares[i]} onSquareClick={() => handleClick(i)} />
+  }
+
+  function renderRow(row) {
+    return (
+      <div className="board-row">
+        {[0,1,2,3,4,5,6,7,8].map((col) => renderSquare(row * 9 + col))}
+      </div>
+    );
+  }
+
+  return(
+    <>
+      {[0,1,2,3,4,5,6,7,8].map((row) => renderRow(row))}
+    </>
+  );
+}
